daf2cdcf34518ed3d4826a18d5d285d755776b3a
abdurrakibrafi <aboudurrakib469@gmail.comm>
2022-03-05 01:33:21 +0600

    code for main

---
+++ src/components/LoginForm.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import Button from "./Button";
+import Form from "./pages/Form";
+import TextInput from "./TextInput";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+
+  const { login } = useAuth();
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(email, password);
+      history.push("/");
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError("Failed to Login");
+    }
+  }
+  return (
+    <Form style={{ height: "300px" }} onSubmit={handleSubmit}>
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button disabled={loading} type="submit">
+        <span>Submit Now</span>
+      </Button>
+
+      {error && <p className="error">{error}</p>}
+
+      <div className="info">
+        Don't have an account? <Link to="/signup">Signup</Link> instead.
+      </div>
+    </Form>
+  );
+}
---
+++ src/components/Summary.js
@@ -0,0 +1,17 @@
+import Image from "../assest/image/success.png";
+import classes from "../styles/Summary.module.css";
+export default function Summary() {
+  return (
+    <div className={classes.summary}>
+      <div className={classes.point}>
+        <p className={classes.score}>
+          Your score is <br />5 out of 10
+        </p>
+      </div>
+
+      <div className={classes.badge}>
+        <img src={Image} alt="Success" />
+      </div>
+    </div>
+  );
+}
